f962c4264e4202c4a15f117e1048ddd98fba2935
Kelechi <Kelechiojelabi@outlook.com>
2021-08-18 15:48:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+document.write('Kelechi Joanna Ojelabi');
+
+
+    const bars = document.querySelectorAll('.progress__bar');
+
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    })
+    console.log(percentage);
